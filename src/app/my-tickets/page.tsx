32f046e4df71c94c785df9f328b396bce7b75702
Dagger <odidihope@gmail.com>
2025-02-15 15:02:11 +0100

    fixed image fetching

--- src/app/my-tickets/page.tsx
+++ src/app/my-tickets/page.tsx
@@ -14,7 +14,7 @@ const TicketPage = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#08252B] border rounded-[32px] border-[#0E464F] text-white">
       <h1 className="text-2xl font-bold mb-4 font-primary">Your Ticket</h1>
       {ticketImage ? (
-        <Image
+        <img
           src={ticketImage}
           alt="Saved Ticket"
           className="w-80 rounded-lg shadow-lg"
